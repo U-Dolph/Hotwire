4587f4681a0120d5b123a61d13935a3e0d80890e
Madarász Tamás <m.tamas1012@gmail.com>
2021-12-15 11:01:26 +0100

    removed hashing from User constructor

--- HotwireServer/models/User.py
+++ HotwireServer/models/User.py
@@ -8,6 +8,6 @@ class User:
         self.username = data['username']
         self.nickname = data['nickname']
         self.nickname_id = 0
-        self.password = hashlib.sha256(data['password'].encode("ascii")).hexdigest()
+        self.password = data['password']
         self.status = 0
         self.time_registered = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
