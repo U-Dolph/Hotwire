f70f23f7465ff73b6ce7d26e9bc35f3a4a96fbe9
Madarász Tamás <m.tamas1012@gmail.com>
2021-12-17 22:38:23 +0100

    now the socket setup requires the JWT

--- HotwireServer/endpoints/setup_socket.py
+++ HotwireServer/endpoints/setup_socket.py
@@ -1,6 +1,8 @@
 import datetime
-import json
-from flask import Blueprint, Response, request
+import uuid
+import jwt
+from flask import Blueprint, request, make_response, jsonify
+from config_holder import conf
 from token_handler import token_required
 
 socket_page = Blueprint('socket_page', __name__)
@@ -11,10 +13,14 @@ ticket_cache = []
 @socket_page.route('/request_socket', methods=['POST'])
 @token_required
 def request_socket():
-    token = json.dumps({
+    data = jwt.decode(request.headers['x-access-token'], conf['secret_key'])
+    token = {
+        "expire_in": (datetime.datetime.now().replace(microsecond=0) + datetime.timedelta(seconds=5)).strftime('%Y-%m-%d %H:%M:%S'),
+        "user_id": data['id'],
+        "id": uuid.uuid4().hex,
         "sender_ip": request.remote_addr,
-        "timestamp": str(datetime.datetime.utcnow()),
-        "expire_in": str(datetime.datetime.utcnow() + datetime.timedelta(seconds=10))})
+        "timestamp": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    }
 
     ticket_cache.append(token)
-    return Response(json.dumps(token), 200)
+    return make_response(jsonify(token), 200)
