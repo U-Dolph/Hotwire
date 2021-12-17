a49a86f35350ea143dacb447cccf43a550338c2e
Madarász Tamás <m.tamas1012@gmail.com>
2021-12-17 05:58:31 +0100

    fixed user not found issue

--- HotwireServer/endpoints/login.py
+++ HotwireServer/endpoints/login.py
@@ -1,5 +1,5 @@
 import datetime
-from flask import Blueprint, request, Response, make_response, jsonify
+from flask import Blueprint, request, make_response, jsonify
 from database_manager import DB_Manager
 import jwt
 from config_holder import conf
@@ -13,9 +13,10 @@ def login():
 
     if 'username' in data and 'password' in data:
         result = DB_Manager.check_credentials(data['username'], data['password'])
-        user = DB_Manager.get_user(data['username'])
 
         if result == "OK":
+            user = DB_Manager.get_user_by_username(data['username'])
+
             if 'stay_logged_in' in data and data['stay_logged_in']:
                 token = jwt.encode({'id': user.id},
                                    conf['secret_key'])
@@ -26,6 +27,6 @@ def login():
 
             return make_response(jsonify({'token': token.decode('UTF-8')}), 201)
 
-        return Response(result, 400)
+        return make_response(jsonify({'reason': result}), 400)
     else:
-        return Response("Missing arguments", 400)
+        return make_response(jsonify({'reason': "Missing arguments"}), 400)
