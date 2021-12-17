bbfb03f39adc32f035debb291b32237eed97d795
Madarász Tamás <m.tamas1012@gmail.com>
2021-12-17 22:39:09 +0100

    now the token manager stores the issued tokens

--- HotwireServer/token_handler.py
+++ HotwireServer/token_handler.py
@@ -3,8 +3,6 @@ import jwt
 from flask import request, jsonify
 from config_holder import conf
 
-tokens_issued = []
-
 
 def token_required(f):
     @wraps(f)
@@ -18,13 +16,12 @@ def token_required(f):
             return jsonify({'message': 'Token is missing !!'}), 401
 
         try:
-
             data = jwt.decode(token, conf['secret_key'])
         except Exception as e:
             return jsonify({
                 'message': 'Token is invalid !!'
             }), 401
-        # returns the current logged in users contex to the routes
+
         return f(*args, **kwargs)
 
     return decorated
