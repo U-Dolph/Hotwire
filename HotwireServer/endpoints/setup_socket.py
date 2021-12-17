e109cfbc85afc9cb41d5cc40126a0916eb966ec6
Madarász Tamás <m.tamas1012@gmail.com>
2021-12-17 05:59:08 +0100

    added socket token creation

--- HotwireServer/endpoints/setup_socket.py
+++ HotwireServer/endpoints/setup_socket.py
@@ -1,12 +1,20 @@
-import jwt
-from flask import Blueprint, request, Response
-from config_holder import conf
+import datetime
+import json
+from flask import Blueprint, Response, request
 from token_handler import token_required
 
 socket_page = Blueprint('socket_page', __name__)
+
+ticket_cache = []
 
 
 @socket_page.route('/request_socket', methods=['POST'])
 @token_required
 def request_socket():
-    return Response("OK", 101)
+    token = json.dumps({
+        "sender_ip": request.remote_addr,
+        "timestamp": str(datetime.datetime.utcnow()),
+        "expire_in": str(datetime.datetime.utcnow() + datetime.timedelta(seconds=10))})
+
+    ticket_cache.append(token)
+    return Response(json.dumps(token), 200)
