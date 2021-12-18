b39d5afe11c5b9e7986b38d5927bc00eebdecdae
GriffinnTNR <knightddc@gmail.com>
2021-12-18 16:16:12 +0100

    Added RegisterViewModel

---
+++ Hotwire/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Hotwire.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotwire.ViewModel
+{
+    public class RegisterViewModel : BaseViewModel
+    {
+        public RelayCommand SwitchToLoginPageCommand { get; }
+        private readonly BaseViewModel _viewModel;
+
+        public RegisterViewModel(BaseViewModel viewModel)
+        {
+            _viewModel = viewModel;
+            SwitchToLoginPageCommand = new RelayCommand(switchToLoginPage);
+        }
+
+        private void switchToLoginPage()
+        {
+            _viewModel.SelectedViewModel = new LoginViewModel(_viewModel);
+        }
+    }
+}
