bba3568e7d44ab4cf850ec9504a780c637455aa7
GriffinnTNR <knightddc@gmail.com>
2021-12-18 13:42:03 +0100

    Added LoginViewModel

--- Hotwire/ViewModel/BaseViewModel.cs
+++ Hotwire/ViewModel/BaseViewModel.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hotwire.Core;
 
 namespace Hotwire.ViewModel
 {
-    public class BaseViewModel
+    public class BaseViewModel : ObservableObject
     {
+        public LoginViewModel LoginVM { get; set; }
+        public RelayCommand ExitCommand { get; set; }
 
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public BaseViewModel()
+        {
+            LoginVM = new LoginViewModel();
+            CurrentView = LoginVM;
+            ExitCommand = new RelayCommand(o =>
+            {
+                System.Windows.Application.Current.Shutdown();
+            });
+        }
     }
 }
