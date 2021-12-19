3b4c9389fd376c777150b52f75b239da8bb7a6f4
GriffinnTNR <knightddc@gmail.com>
2021-12-19 13:52:25 +0100

    Added AddFriendViewModel

---
+++ Hotwire/ViewModel/AddFriendViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Hotwire.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotwire.ViewModel
+{
+    public class AddFriendViewModel : BaseViewModel
+    {
+        private readonly BaseViewModel viewModel;
+
+        public AddFriendViewModel(BaseViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+    }
+}
