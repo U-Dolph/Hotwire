ff06bd33067ecced33b8868a8ca3dc5177832a26
Madarász Tamás <m.tamas1012@gmail.com>
2021-12-19 22:45:06 +0100

    changed port number to match the real servers port

--- HotwireServer/main.py
+++ HotwireServer/main.py
@@ -2,4 +2,4 @@ from socket_manager import app_socket
 from app import app
 
 if __name__ == '__main__':
-    app_socket.run(app, host='0.0.0.0', port=5000, debug=True)
+    app_socket.run(app, host='0.0.0.0', port=1234, debug=True)
