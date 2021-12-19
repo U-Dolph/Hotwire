7becae195b0e53dd9e9af55ea011cda1891901fe
GriffinnTNR <knightddc@gmail.com>
2021-12-19 13:52:40 +0100

    Added ChatViewModel

---
+++ Hotwire/ViewModel/ChatViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Hotwire.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotwire.ViewModel
+{
+    public class ChatViewModel : BaseViewModel
+    {
+        public RelayCommand SwitchToAddFriendPageCommand { get; }
+        private readonly BaseViewModel viewModel;
+
+        public ChatViewModel(BaseViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        private void switchToAddFriendPage()
+        {
+            viewModel.SelectedViewModel = new AddFriendViewModel(viewModel);
+        }
+    }
+}
