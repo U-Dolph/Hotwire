1f528c2dd581e2ca565f366404c4d4e8efb8d53b
Madarász Tamás <m.tamas1012@gmail.com>
2021-12-20 01:44:23 +0100

    Added constants for the server access

---
+++ Hotwire/Model/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotwire.Model
+{
+    public class Constants
+    {
+        //private static string serverUrl = "http://dolphvault.duckdns.org:1234/";
+        private static string serverUrl = "http://localhost:1234/";
+
+        public static string LoginUrl { get => serverUrl + "login"; }
+        public static string RegisterUrl { get => serverUrl + "register"; }
+        public static string SocketUrl { get => serverUrl + "request_socket"; }
+    }
+}
