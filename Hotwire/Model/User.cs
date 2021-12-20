3e0574020fa0a05d2c3d71a184c604adbda55c5a
Madarász Tamás <m.tamas1012@gmail.com>
2021-12-20 01:44:37 +0100

    Implemented User model

--- Hotwire/Model/User.cs
+++ Hotwire/Model/User.cs
@@ -8,6 +8,25 @@ namespace Hotwire.Model
 {
     public class User
     {
+        public string Username { get; set; }
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+        public bool StayLoggedIn { get; set; }
 
+        //For registering
+        public User(string username, string nickname, string password)
+        {
+            Username = username;
+            Nickname = nickname;
+            Password = password;
+        }
+
+        //For logging in
+        public User(string username, string password, bool stay_logged_in)
+        {
+            Username = username;
+            Password = password;
+            StayLoggedIn = stay_logged_in;
+        }
     }
 }
