75796c118da5420a2daa6017d04614d24735bf4b
Madarász Tamás <m.tamas1012@gmail.com>
2021-12-20 01:45:10 +0100

    Implemented registration in the http service

--- Hotwire/Services/HttpService.cs
+++ Hotwire/Services/HttpService.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Hotwire.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,27 @@ namespace Hotwire.Services
 {
     public class HttpService
     {
+        private HttpClient client;
+
+        public HttpService()
+        {
+            client = new HttpClient();
+        }
+        
+        public async Task<string> RegisterUser(User user)
+        {
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+            try
+            {
+                HttpResponseMessage msg = await client.PostAsync(Constants.RegisterUrl, content);
+
+                return msg.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
     }
 }
